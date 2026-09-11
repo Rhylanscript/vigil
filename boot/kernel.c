bb75da8cfe6512a4e1f28b6fc6fe205356d09fce
Rhylanscript <181093569+Rhylanscript@users.noreply.github.com>
2026-09-11 23:21:45 +1000

    feat: add terminal driver with initialization and printing functions

--- boot/kernel.c
+++ boot/kernel.c
@@ -1,12 +1,8 @@
-static unsigned short* const VGA_MEMORY = (unsigned short*) 0xb8000;
+#include "terminal.h"
 
 void kernel_main(void) {
-    const char* message = "VIGIL kernal online";
-    unsigned char colour = 0x0f;
-
-    for (int i = 0; message[i] != '\0'; i++) {
-        VGA_MEMORY[i] = (unsigned short) message[i] | ((unsigned short) colour << 8);
-    }
+    terminal_initialize();
+    terminal_print("VIGIL kernal online\n");
 
     for (;;) {
         
