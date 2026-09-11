b2ee3b743b205887e62583dcd28855c2f31e8d03
Rhylanscript <181093569+Rhylanscript@users.noreply.github.com>
2026-09-11 23:24:34 +1000

    chore: update run script to compile and link terminal driver

--- run.bat
+++ run.bat
@@ -12,10 +12,14 @@ echo Compiling kernel.c...
 i686-elf-gcc -ffreestanding -c boot/kernel.c -o boot/kernel.o
 if errorlevel 1 goto :error
 
+echo Compiling terminal.c...
+i686-elf-gcc -ffreestanding -c boot/terminal.c -o boot/terminal.o
+if errorlevel 1 goto :error
+
 if not exist build mkdir build
 
-echo Linking stage2.o and kernel.o...
-i686-elf-ld -T boot/linker.ld -o build/kernel_full.elf boot/stage2.o boot/kernel.o
+echo Linking stage2.o, kernel.o, and terminal.o...
+i686-elf-ld -T boot/linker.ld -o build/kernel_full.elf boot/stage2.o boot/kernel.o boot/terminal.o
 if errorlevel 1 goto :error
 
 echo Converting to flat binary...
