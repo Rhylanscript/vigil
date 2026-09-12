77a926e034c2d9d03cdc0eef51191de75cac42e8
Rhylanscript <181093569+Rhylanscript@users.noreply.github.com>
2026-09-12 20:23:47 +1000

    feat: enhance keyboard driver with shift and caps lock functionality, align cursor to text, add backspace functionality

--- boot/drivers/keyboard.c
+++ boot/drivers/keyboard.c
@@ -1,4 +1,4 @@
-// boot/keyboard.c
+// boot/drivers/keyboard.c
 
 #include "keyboard.h"
 #include "irq.h"
@@ -22,16 +22,62 @@ static const char scancode_to_ascii[128] = {
     ' '
 };
 
+static const char scancode_to_ascii_shifted[128] = {
+    0, 27,
+    '!','@','#','$','%','^','&','*','(',')','_','+',
+    '\b',
+    '\t',
+    'Q','W','E','R','T','Y','U','I','O','P','{','}',
+    '\n',
+    0,
+    'A','S','D','F','G','H','J','K','L',':','"','~',
+    0,
+    '|','Z','X','C','V','B','N','M','<','>','?',
+    0,
+    '*',
+    0,
+    ' '
+};
+
+#define SC_LSHIFT_PRESS   0x2a
+#define SC_RSHIFT_PRESS   0x36
+#define SC_LSHIFT_RELEASE 0xaa
+#define SC_RSHIFT_RELEASE 0xb6
+#define SC_CAPSLOCK_PRESS 0x3a
+
+static int shift_held = 0;
+static int caps_active = 0;
+
 static void keyboard_callback(struct registers regs) {
     (void) regs;
 
     unsigned char scancode = inb(0x60);
 
+    if (scancode == SC_LSHIFT_PRESS || scancode == SC_RSHIFT_PRESS) {
+        shift_held = 1;
+        return;
+    }
+    if (scancode == SC_LSHIFT_RELEASE || scancode == SC_RSHIFT_RELEASE) {
+        shift_held = 0;
+        return;
+    }
+    if (scancode == SC_CAPSLOCK_PRESS) {
+        caps_active = !caps_active;
+        return;
+    }
+
     if (scancode & 0x80) {
         return;
     }
 
-    char c = scancode_to_ascii[scancode];
+    char lower = scancode_to_ascii[scancode];
+    int use_shifted = shift_held;
+
+    if (caps_active && lower >= 'a' && lower <= 'z') {
+        use_shifted = !use_shifted;
+    }
+
+    char c = use_shifted ? scancode_to_ascii_shifted[scancode] : lower;
     if (c != 0) {
         terminal_putchar(c);
     }
--- boot/drivers/terminal.c
+++ boot/drivers/terminal.c
@@ -1,17 +1,29 @@
-// boot/terminal.c
+// boot/drivers/terminal.c
 
 #include "terminal.h"
+#include "io.h"
 
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((unsigned short*) 0xb8000)
 #define WHITE_ON_BLACK 0x0f
+#define VGA_CTRL_PORT 0x3d4
+#define VGA_DATA_PORT 0x3d5 
 
 static int cursor_row;
 static int cursor_col;
 
 static unsigned short make_cell(char c, unsigned char colour) {
     return (unsigned short) c | ((unsigned short) colour << 8);
+}
+
+static void update_cursor(void) {
+    unsigned short pos = cursor_row * VGA_WIDTH + cursor_col;
+
+    outb(VGA_CTRL_PORT, 0x0f);
+    outb(VGA_DATA_PORT, (unsigned char) (pos & 0xff));
+    outb(VGA_CTRL_PORT, 0x0e);
+    outb(VGA_DATA_PORT, (unsigned char) ((pos >> 8) & 0xff));
 }
 
 void terminal_initialize(void) {
@@ -23,6 +35,7 @@ void terminal_initialize(void) {
 
     cursor_row = 0;
     cursor_col = 0;
+    update_cursor();
 }
 
 static void scroll_if_needed(void) {
@@ -43,6 +56,7 @@ static void scroll_if_needed(void) {
     }
 
     cursor_row = VGA_HEIGHT - 1;
+    update_cursor();
 }
 
 void terminal_putchar(char c) {
@@ -52,6 +66,19 @@ void terminal_putchar(char c) {
         cursor_col = 0;
         cursor_row++;
         scroll_if_needed();
+        update_cursor();
+        return;
+    }
+
+    if (c == '\b') {
+        if (cursor_col > 0) {
+            cursor_col--;
+        } else if (cursor_row > 0) {
+            cursor_row--;
+            cursor_col = VGA_WIDTH - 1;
+        }
+        video[cursor_row * VGA_WIDTH + cursor_col] = make_cell(' ', WHITE_ON_BLACK);
+        update_cursor();
         return;
     }
 
@@ -63,6 +90,7 @@ void terminal_putchar(char c) {
         cursor_row++;
         scroll_if_needed();
     }
+    update_cursor();
 }
 
 void terminal_print(const char* str) {
